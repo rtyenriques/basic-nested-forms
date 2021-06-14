61bc9e272c65eebbe9b4f1129aa1da322e384ac5
= <rtyenriques@gmail.com>
2021-06-14 00:29:08 -0700

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,9 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
-
   def create    
     Person.create(person_params)
     redirect_to people_path
